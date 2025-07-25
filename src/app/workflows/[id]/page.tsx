80186f1104e3cda74e9fd93baa33e98048a8457b
a745839lin <18602236204@163.com>
2025-07-25 21:29:16 +0800

    555
    666

--- src/app/workflows/[id]/page.tsx
+++ src/app/workflows/[id]/page.tsx
@@ -1,9 +1,16 @@
 import { redirect } from 'next/navigation'
+import { workflowsData } from '@/data/workflowsData'
 
 interface PageProps {
   params: {
     id: string
   }
+}
+
+export async function generateStaticParams() {
+  return Object.keys(workflowsData).map((id) => ({
+    id,
+  }))
 }
 
 export default function WorkflowPage({ params }: PageProps) {
